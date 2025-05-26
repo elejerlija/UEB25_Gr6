22d78848a807397a2041bdfae3a06e5f68f7bd1f
elejerlija <elajerlija3@gmail.com>
2025-05-26 22:23:55 +0200

    Update

--- includes/count.php
+++ includes/count.php
@@ -3,9 +3,7 @@
 if (session_status() === PHP_SESSION_NONE) session_start();
 
 
-if (!isset($conn)) {
-    include __DIR__ . 'includes/db_conn.php';
-}
+
 
 if (isset($_SESSION['id'])) {
     $user_id = $_SESSION['id'];
